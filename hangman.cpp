1e016d52999fa99ca4debb4e9863d56f1fa7fefe
Zurxae <59427397+Zurxae@users.noreply.github.com>
2021-08-18 20:00:56 -0400

    re-organized into class

--- hangman.cpp
+++ hangman.cpp
@@ -4,35 +4,62 @@
 
 using namespace std;
 
-int main()
+class hangman
 {
-    int size = 1;
-    int count = 0;
-    string *animalList = new string[size];
+public:
+    hangman();
+    ~hangman();
+    void doubleSize();
+    bool isFull();
+
+private:
+    int size;
+    int count;
+    string *animalList;
+};
+
+hangman::hangman()
+{
+    size = 1;
+    count = 0;
+    animalList = new string[size];
+
     ifstream in;
     in.open("animals.txt");
+
     while (!in.eof())
     {
-        if (size == count)
-        {
-            size *= 2;
-            string *temp = new string[size];
-            for (int i = 0; i < count; i++)
-            {
-                temp[i] = animalList[i];
-            }
-            animalList = temp;
-            delete[] temp;
-        }
+        if (isFull())
+            doubleSize();
         getline(in, animalList[count]);
         cout << animalList[count] << endl;
         count++;
     }
     in.close();
-    //for (int i = 0; i < 20; i++)
-    //{
-    //    cout << animalList[i] << endl;
-    //}
+    for (int i = 0; i < 20; i++)
+    {
+        cout << animalList[i] << endl;
+    }
+}
+
+void hangman::doubleSize()
+{
+    size *= 2;
+    string *temp = new string[size];
+    for (int i = 0; i < count; i++)
+    {
+        temp[i] = animalList[i];
+    }
+    animalList = temp;
+}
+
+bool hangman::isFull()
+{
+    return size == count;
+}
+
+int main()
+{
 
     return 0;
 }
